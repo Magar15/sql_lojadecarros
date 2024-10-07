d75e2f20d247f15a5bad8bedee0f87eea26efa53
Gabriel Magalhães <125617287+Magar15@users.noreply.github.com>
2024-10-06 22:52:05 -0300

    Add files via upload
    MYSQL do app

---
+++ concess.sql
@@ -0,0 +1,39 @@
+use retrocars;
+
+/*
+create table cliente (
+	id int not null auto_increment,
+    nome varchar(100) not null,
+    cep numeric(8) not null,
+    cpf numeric(11) not null,
+    telefone numeric(11) not null,
+    primary key (id)
+);
+
+
+create table vendedores (
+	id int not null auto_increment,
+    nome varchar(100) not null,
+    cpf numeric(11) not null,
+    cep numeric(8) not null,
+    data_nascimento varchar(20) not null,
+    vendas numeric(6),
+    primary key (id)
+);
+
+
+create table estoque (
+	id int not null auto_increment,
+    marca varchar(20) not null,
+    modelo varchar(20) not null,
+    ano numeric(4) not null,
+    cor varchar(15) not null,
+    categoria varchar(20) not null,
+    preço numeric(10) not null,
+    quantidade varchar(3),
+    primary key (id)
+);
+*/
+
+
+estoqueactor
